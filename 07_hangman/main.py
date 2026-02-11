8ef3461a0540b9c388a0ab02449aecf94d94d6bf
tbindewald96 <tobias.bindewald96@gmx.de>
2026-02-11 15:57:37 +0000

    Finalize Hangman Project

--- 07_hangman/main.py
+++ 07_hangman/main.py
@@ -2,35 +2,41 @@ import random
 from hangman_art import HANGMANPICS
 
 word_list = ["aardvark", "baboon", "camel"]
+word = list(random.choice(word_list))
 placeholder_word = []
-hangman = HANGMANPICS[0]
-lives = 7
-word = random.choice(word_list)
-
-#Create placeholder with same number of blanks as choice
-
 number_of_placeholders = len(word)
+lives = 7
+points = 0
+hangman = 0
+guessed_letters = []
 
 for i in range(number_of_placeholders):
     placeholder_word.append("_")
 
-print(placeholder_word)
-
-guess = input("Guess a letter.\n").lower()
+print(''.join(placeholder_word))
 
 while lives != 0:
-    if guess in word:
-        print("Your guess is correct!")
-        location_of_letter = word.find(guess)
-        placeholder_word[location_of_letter] = guess
-        print(placeholder_word)
-    else:
-        print("Your guess is incorrect!")
-        lives -= 1
-        print(hangman)
-
-print("You lost!")
-
-
-# Create a display with letter at correct location
+    guess = input("Guess a letter.\n").lower()
+    if guess not in guessed_letters:
+        if guess in word:
+            print("Your guess is correct!")
+            guessed_letters.append(guess)
+            for i in range(number_of_placeholders):
+                if word[i] == guess:
+                    placeholder_word[i] = guess
+            print(''.join(placeholder_word))
+            if placeholder_word == word:
+                print("You won!")
+                break
+        else:
+            print("Your guess is incorrect!")
+            guessed_letters.append(guess)
+            print(HANGMANPICS[hangman])
+            hangman += 1
+            lives -= 1
+            if lives == 0:
+                print("You lost!")
+                break
+    elif guess in guessed_letters:
+        print("You already guessed this letter.")
 
