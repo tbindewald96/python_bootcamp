371e9ed45af04f05d976271a436cffaba95ecc56
tbindewald96 <tobias.bindewald96@gmx.de>
2026-02-12 09:13:56 +0000

    Learn about function inputs

---
+++ 08_function_parameters_and_caesar_cipher/task.py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hey there!")
+    print("It's nice to meet you!")
+    print("How are you?")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hey there {name}!")
+    print(f"It's nice to meet you {name}!")
+    print("How are you?")
+
+greet_with_name("Tobi")
