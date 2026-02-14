5732a7a12211541c04a942375a0f85fc74e27442
tbindewald96 <tobias.bindewald96@gmx.de>
2026-02-14 10:19:40 +0000

    Continue on the cypher project

--- 08_function_parameters_and_caesar_cipher/main.py
+++ 08_function_parameters_and_caesar_cipher/main.py
@@ -1,17 +1,35 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
+text = list(input("Type your message:\n").lower())
 shift = int(input("Type the shift number:\n"))
+encrypted_list =  []
+decrypted_list = []
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+if direction == "encode":
+    def encrypt(original_text, shift_amount):
+        for i in original_text:
+            location = alphabet.index(i)
+            new_location = location + shift
+            if new_location > 25: 
+              new_location = new_location - 26
+            encrypted_list.append(alphabet[new_location])
+        encrypted_word = ''.join(encrypted_list)
+        return f"The encrypted word is: {encrypted_word}"
+    print(encrypt(text,shift))
+elif direction == "decode":
+    def decrypt(original_text, shift_amount):
+        for i in original_text:
+            location = alphabet.index(i)
+            new_location = location - shift
+            decrypted_list.append(alphabet[new_location])
+        decrypted_word = ''.join(decrypted_list)
+        return f"The decrypted word is: {decrypted_word}"
+    print(decrypt(text,shift))
 
-def encrypt(original_text, shift_amount):
-    new_text = 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+  
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+#  by the shift amount and print the decrypted text.
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
