281e5a2877fa611be1f6da5391fbd71f9babab8c
tbindewald96 <tobias.bindewald96@gmx.de>
2026-02-15 09:27:33 +0000

    Finish caesar cipher project

--- 08_function_parameters_and_caesar_cipher/main.py
+++ 08_function_parameters_and_caesar_cipher/main.py
@@ -1,35 +1,43 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = list(input("Type your message:\n").lower())
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+done = False
 encrypted_list =  []
 decrypted_list = []
 
-if direction == "encode":
-    def encrypt(original_text, shift_amount):
-        for i in original_text:
-            location = alphabet.index(i)
-            new_location = location + shift
-            if new_location > 25: 
-              new_location = new_location - 26
-            encrypted_list.append(alphabet[new_location])
-        encrypted_word = ''.join(encrypted_list)
-        return f"The encrypted word is: {encrypted_word}"
-    print(encrypt(text,shift))
-elif direction == "decode":
-    def decrypt(original_text, shift_amount):
-        for i in original_text:
-            location = alphabet.index(i)
-            new_location = location - shift
-            decrypted_list.append(alphabet[new_location])
-        decrypted_word = ''.join(decrypted_list)
-        return f"The decrypted word is: {decrypted_word}"
-    print(decrypt(text,shift))
+print(logo)
 
-  
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+while not done: 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = list(input("Type your message:\n").lower())
+    shift = int(input("Type the shift number:\n"))
 
+    def caesar(original_text, shift_amount):
+        if direction == "encode":
+            for i in original_text:
+                if i in alphabet:
+                    location = alphabet.index(i)
+                    new_location = location + shift
+                    if new_location > 25: 
+                        new_location = new_location % 26
+                    encrypted_list.append(alphabet[new_location])
+                else: 
+                    encrypted_list.append(i)
+            encrypted_word = ''.join(encrypted_list)
+            return f"The encrypted word is: {encrypted_word}"
+        elif direction == "decode":
+            for i in original_text:
+                if i in alphabet:
+                    location = alphabet.index(i)
+                    new_location = location - shift
+                    if new_location < 0:
+                        new_location = 26 - (-new_location % 26)
+                    decrypted_list.append(alphabet[new_location])
+                else:
+                    decrypted_list.append(i)
+            decrypted_word = ''.join(decrypted_list)
+            return f"The decrypted word is: {decrypted_word}"
+    print(caesar(text, shift))
+    proceed = input('Do you want to continue? Type "yes" or "no".\n').lower()
+    if proceed == "no":
+        done = True
