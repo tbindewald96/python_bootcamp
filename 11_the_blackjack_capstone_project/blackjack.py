868b60c19182588d62758b2c297f140577d7403e
tbindewald96 <tobias.bindewald96@gmx.de>
2026-02-21 11:27:10 +0000

    Continue on blackjack project

--- 11_the_blackjack_capstone_project/blackjack.py
+++ 11_the_blackjack_capstone_project/blackjack.py
@@ -6,38 +6,50 @@ print(logo)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 computer_hand = []
 player_hand = []
+done = ()
 
 def random_choice():
     return random.choice(cards)
 
-#The deck is unlimited in size.
-#There are no jokers.
-#The Jack/Queen/King all count as 10.
-#The Ace can count as 11 or 1.
-#Use the following list as the deck of cards:
-#The cards in the list have equal probability of being drawn.
-#Cards are not removed from the deck as they are drawn.
-#The computer is the dealer.
-
 start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
 if start_game == "y":
-    done = False
+    done == True
 
-while not done:
-    for i in range(2):
-        player_hand.append(random_choice())
+# First Round
+for i in range(2):
+    player_hand.append(random_choice())
+player_score = sum(player_hand)
+for i in range(2):
+    computer_hand.append(random_choice())
+computer_score = sum(computer_hand)
+print(f"Your cards: {player_hand}, current score: {player_score}")
+print(f"Computer's first card: {str(computer_hand[0])}")
+
+# Consecutive Rounds; Player hits as many times as he likes
+while player_score < 21 and not done:
+    proceed = input("Type 'y' to get another card, type 'n' to pass: ")
+    if proceed == "n":
+        done = True
+    player_hand.append(random_choice())
     player_score = sum(player_hand)
-    computer_first_card = random_choice()
-    computer_hand.append(computer_first_card)
     print(f"Your cards: {player_hand}, current score: {player_score}")
-    print(f"Computer's first card: {computer_first_card}")
-    while player_score < 21 and not done:
-        proceed = input("Type 'y' to get another card, type 'n' to pass: ")
-        if proceed == "n":
-            done = True
-        print(done)
-        player_hand.append(random_choice)
+    print(f"Computer's first card: {str(computer_hand[0])}")
+
+# Dealer has to hit if his cards are below 17
+
+while computer_score < 17:
+    computer_hand.append(random_choice())
+    computer_score = sum(computer_hand)
+
+# End Evaluation
+if player_score == 21:
+    print("You won!")
+elif player_score == computer_score and player_score <= 21:
+    print("Draw.")
+elif player_score > 21:
+    print("You lose!")
+
         
 
 
