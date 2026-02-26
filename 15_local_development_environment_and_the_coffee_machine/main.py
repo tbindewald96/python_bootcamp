86fbe06aa136e333efb77f28f8f1a8aae87251d1
tbindewald96 <tobias.bindewald96@gmx.de>
2026-02-26 12:52:39 +0000

    Create the budget function; introduce the off button

--- 15_local_development_environment_and_the_coffee_machine/main.py
+++ 15_local_development_environment_and_the_coffee_machine/main.py
@@ -30,6 +30,42 @@ resources = {
     "coffee": 100,
 }
 
+coins_value = {
+    "penny": 0.01,
+    "nickel": 0.05,
+    "dime": 0.1,
+    "quarter": 0.25,
+}
+
+coins_amount = {
+    "penny": 1,
+    "nickel": 1,
+    "dime": 1,
+    "quarter": 1, 
+}
+
+def budget():
+  coins = ["penny", "nickel", "dime", "quarter"]
+  values = []
+  for i in coins:
+    values.append(coins_amount[i] * coins_value[i])
+  return round(sum(values), 2)
+
+
 # TODO: 1. Print report of all resources
 # TODO: 2. Check resources sufficient to make order
 # TODO: 3: Process coins
+
+while True:
+    button = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if button == "report":
+            print(f"Water: {resources["water"]}ml")
+            print(f"Milk: {resources["milk"]}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: ${budget()}")
+    elif button == "espresso" or button == "latte" or button == "cappucino":
+        
+    elif button == "off":
+        break
+
